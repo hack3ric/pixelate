0434b5085d94bd91f6712aad1892210d4704e29d
Eric Long <hackereric@outlook.com>
2021-07-14 10:44:30 +0800

    Switch back from Preact to React

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  reactStrictMode: true,
-  webpack(config, { dev, isServer }) {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        "react": "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      })
-    }
-    return config
-  }
+  reactStrictMode: true
 }
